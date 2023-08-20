8d5177a73155ed07090d75b3d200b77755897a92
YungMilky <filtersome@gmail.com>
2023-08-20 11:59:04 +0200

    email style

--- src/components/emailDialog.tsx
+++ src/components/emailDialog.tsx
@@ -1,12 +1,7 @@
 import emailjs from '@emailjs/browser';
-import {
-  forwardRef,
-  useRef,
-  Ref,
-  ForwardedRef,
-  useEffect,
-  useState,
-} from 'react';
+import { useRef, useEffect, useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+import Switch from 'react-switch';
 import { EmailData } from '../ts/interfaces';
 import styles from '../styles/form.module.css';
 
@@ -57,46 +52,68 @@ const EmailDialog = ({
   return (
     <dialog className={[styles.formCard, styles.dialog].join(' ')} ref={ref}>
       <form method='dialog'>
-        <div className={styles.row}>
-          <label htmlFor='recipient'>Till:</label>
-          <input
-            name='recipient'
-            type='email'
-            placeholder='example@gmail.com'
-            value={recipient}
-            onChange={({ target }) => setRecipient(target?.value)}
-            required
-          />
+        <div className={[styles.column, styles.emailBody].join(' ')}>
+          <div className={styles.row}>
+            <label htmlFor='recipient'>Till:</label>
+            <input
+              autoFocus
+              name='recipient'
+              type='email'
+              placeholder='example@gmail.com'
+              value={recipient}
+              onChange={({ target }) => setRecipient(target?.value)}
+              required
+            />
+          </div>
+          <div className={[styles.row, styles.emailFirstLine].join(' ')}>
+            <label htmlFor='name'>Hej</label>
+            <span
+              className={styles.fakeInput}
+              contentEditable={true}
+              placeholder='Sam'
+              onChange={({ currentTarget }) =>
+                setName(currentTarget?.innerText)
+              }
+            >
+              {name}
+            </span>
+            <label id={styles.firstLineComma} htmlFor='name'>
+              ,
+            </label>
+          </div>
+
+          <div className={styles.row}>
+            <TextareaAutosize
+              aria-label='Write your email body'
+              name='emailBody'
+              placeholder={`Min faktura är bifogad nedan.`}
+              value={emailBody}
+              onChange={({ target }) => setEmailBody(target?.value)}
+            />
+          </div>
         </div>
-        <div className={styles.row}>
-          <label htmlFor='name'>Hej</label>
-          <input
-            name='name'
-            type='text'
-            value={name}
-            placeholder='Sam'
-            onChange={({ target }) => setName(target?.value)}
-            required
-          />
-          ,
-        </div>
-        <div className={styles.row}>
-          <textarea
-            aria-label='Write your email body'
-            name='emailBody'
-            placeholder='Min faktura är bifogad nedan.'
-            value={emailBody}
-            onChange={({ target }) => setEmailBody(target?.value)}
-          />
-        </div>
-        <div className={styles.row}>
-          <input
-            name='bcc'
-            type='checkbox'
-            checked={bcc}
-            onChange={() => setBcc(!bcc)}
-          />
-          <label htmlFor='bcc'>Skicka en kopia till mig</label>
+        <div onClick={() => setBcc(!bcc)}>
+          <label htmlFor='bcc'>
+            <Switch
+              onColor='#617A55'
+              height={20}
+              width={38}
+              handleDiameter={12}
+              offColor='#333333'
+              offHandleColor='#7b7b7b'
+              checkedIcon={false}
+              uncheckedIcon={false}
+              className={[styles.bccCheckbox, bcc && 'bccCheckboxChecked'].join(
+                ' '
+              )}
+              borderRadius={3}
+              name='bcc'
+              type='checkbox'
+              checked={bcc}
+              onChange={() => {}}
+            />
+            <span> Skicka en kopia till mig</span>
+          </label>
         </div>
         <div className={[styles.row, styles.buttonGroup].join(' ')}>
           <button
